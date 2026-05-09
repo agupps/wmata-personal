bba0ba3ab0c2a535146df87cdef5b26653b3d18c
Aditya Gupta <adityagupta2017@gmail.com>
2026-05-09 11:18:58 -0400

    start moving things out to other packages

---
+++ internal/buses/bus.go
@@ -0,0 +1,86 @@
+package buses
+
+import (
+	"net/http"
+	"fmt"
+	"log"
+	"io"
+	"encoding/json"
+	"github.com/spf13/viper"
+	"wmata/internal/transit"
+)
+
+
+func BusStops(stopNumber int) string {
+	url := "https://api.wmata.com/NextBusService.svc/json/jPredictions?StopID=%d"
+
+	apiKey := viper.GetString("api.key")
+
+	request, _ := http.NewRequest("GET", fmt.Sprintf(url, stopNumber), nil)
+	request.Header.Set("Cache-Control", "no-cache")
+	request.Header.Set("api_key", apiKey)
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		log.Fatalf("failed to hit API: %v", err)
+	}
+	defer response.Body.Close()
+
+	// Read and print the response body
+	body, _ := io.ReadAll(response.Body)
+
+	var nbs NextBusServiceResponse
+	err = json.Unmarshal(body, &nbs)
+	if err != nil {
+		fmt.Println("%v",err)
+		return ""
+	}
+
+	d72Predictions := []BusPrediction{}
+
+	for _, prediction := range nbs.Predictions {
+		if prediction.RouteID == "D72" {
+			d72Predictions = append(d72Predictions, *prediction)
+		}
+	}
+
+	return transit.PredictionsToString(d72Predictions)
+}
+
+func predictionsToString(predictions []*BusPrediction) string {
+	out := ""
+	for _, p := range predictions {
+		out += p.GetPrediction()
+	}
+	return out
+}
+
+
+type NextBusServiceResponse struct {
+	StopName string `json:"StopName"`
+	Predictions []*BusPrediction `json:"Predictions"`
+}
+
+type BusPrediction struct {
+	RouteID string `json:"RouteID"`
+	DirectionText string `json:"DirectionText"`
+	Minutes int `json:"Minutes"`
+}
+
+func (b *BusPrediction) GetMinutes() string {
+	return string(b.Minutes);
+}
+
+func (b *BusPrediction) GetDirectionText() string {
+	return b.DirectionText;
+}
+
+func (b *BusPrediction) GetRouteID() string {
+	return b.RouteID;
+}
+
+func (b *BusPrediction) GetPrediction() string {
+  return fmt.Sprintf("%s %s: %d\n", b.RouteID, b.DirectionText, b.Minutes)
+}
+
---
+++ internal/metro/metro.go
@@ -0,0 +1,81 @@
+package metro
+
+import (
+	"net/http"
+	"fmt"
+	"log"
+	"io"
+	"encoding/json"
+	"github.com/spf13/viper"
+	"wmata/internal/transit"
+)
+
+func MetroStops(metroStop string) string {
+	// metro stations
+  
+	apiKey := viper.GetString("api.key")
+
+	url := "https://api.wmata.com/StationPrediction.svc/json/GetPrediction/%s"
+
+	request, _ := http.NewRequest("GET", fmt.Sprintf(url, metroStop), nil)
+	request.Header.Set("Cache-Control", "no-cache")
+	request.Header.Set("api_key", apiKey)
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		log.Fatalf("failed to hit API: %v", err)
+	}
+	defer response.Body.Close()
+
+	body, _ := io.ReadAll(response.Body)
+
+	var sp StationResponse 
+	err = json.Unmarshal(body, &sp)
+	if err != nil {
+		fmt.Println("%v", err)
+		return ""
+	}
+
+	silverLineTrainPredictions := []TrainPrediction{}
+
+	for _, train := range sp.Trains {
+		if train.Line == "SV" {
+			silverLineTrainPredictions = append(silverLineTrainPredictions, *train)
+		}
+	}
+	return transit.PredictionsToString(silverLineTrainPredictions)
+}
+
+type StationResponse struct {
+	Trains []*TrainPrediction `json:Trains`
+}
+
+type TrainPrediction struct {
+	Cars string `json:"Car"`
+	Destination string `json:"Destination"`
+	DestinationCode string `json:"DestinationCode"`
+	DestinationName string `json:"DestinationName"`
+	Group string `json:"Group"`
+	Line string `json:"Line"`
+	LocationCode string `json:"LocationCode"`
+	LocationName string `json:"LocationName"`
+	Minutes string `json:"Min"`
+}
+
+func (t *TrainPrediction) GetMinutes() string {
+	return t.Minutes
+}
+
+func (t *TrainPrediction) GetDestinationText() string {
+	return t.DestinationName
+}
+
+func (t *TrainPrediction) GetRouteID() string {
+	return t.Line
+}
+
+func (t *TrainPrediction) GetPrediction() string {
+  return fmt.Sprintf("%s to %s: %s\n", t.Line, t.DestinationName, t.Minutes)
+}
+
